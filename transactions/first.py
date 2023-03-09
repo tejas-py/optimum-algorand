07cda619aa2267f4618b4043a47b56e7f925b28b
Tejas Parikh <tejas@evident.capital>
2023-03-09 21:52:45 +0530

    Smart Contract Structure complete.
    TODO:
    Testing Application and Functions
    See How to call the off chain application function using beaker.

--- transactions/first.py
+++ transactions/first.py
@@ -1,20 +1,50 @@
-from algosdk.atomic_transaction_composer import *
-from algosdk import transaction, mnemonic, account, encoding
-import connection
-import application
+from algosdk.atomic_transaction_composer import AtomicTransactionComposer, AccountTransactionSigner
+from algosdk import transaction, encoding
+from connection import algo_conn
+from application import Optimum
 from beaker import client
 
 # Connect to Algod-Client
-algod_client = connection.algo_conn("testnet")
+algod_client = algo_conn("testnet")
 # Create a Dummy Signer to fetch the transaction object
 ACCOUNT_SIGNER = AccountTransactionSigner("a" * 32)
+
+
+# opt in transaction
+def app_optin(app_id, wallet_address):
+
+    # Create  an app client for our app
+    app_client = client.ApplicationClient(
+        client=algod_client, app=Optimum(), app_id=app_id, signer=ACCOUNT_SIGNER
+    )
+
+    params = algod_client.suggested_params()
+    params.fee = 1000
+    params.flat_fee = True
+
+    # build the transaction
+    atc = AtomicTransactionComposer()
+    app_client.add_transaction(
+        atc=atc,
+        txn=transaction.ApplicationOptInTxn(
+            wallet_address,
+            params,
+            app_id
+        )
+    )
+
+    # extract the transaction from the ATC
+    txn_details = atc.txn_list[0]
+    result = [{'txn': encoding.msgpack_encode(txn_details.txn)}]
+
+    return result
 
 
 def call_vrf(app_id, wallet_address):
 
     # Create  an app client for our app
     app_client = client.ApplicationClient(
-        client=algod_client, app=application.Optimum(), app_id=app_id, signer=ACCOUNT_SIGNER
+        client=algod_client, app=Optimum(), app_id=app_id, signer=ACCOUNT_SIGNER
     )
 
     params = algod_client.suggested_params()
@@ -25,7 +55,7 @@ def call_vrf(app_id, wallet_address):
     atc = AtomicTransactionComposer()
     app_client.add_method_call(
         atc=atc,
-        method=application.Optimum.VRF,
+        method=Optimum.VRF,
         sender=wallet_address,
         suggested_params=params,
         foreign_apps=[110096026]
@@ -40,4 +70,4 @@ def call_vrf(app_id, wallet_address):
 
 if __name__ == "__main__":
 
-    call_vrf(16265514, "Z7P757RUNGXLTFLFLSYQW3VHVAFWNIEJFY7ZF5B6BMZ6HLYVDXL3JS2QVA")
+    app_optin(16265514, "Z7P757RUNGXLTFLFLSYQW3VHVAFWNIEJFY7ZF5B6BMZ6HLYVDXL3JS2QVA")
