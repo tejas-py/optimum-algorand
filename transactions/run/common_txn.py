f8063c4c0275c69a4e15624ec8701817d9fed53b
Tejas Parikh <tejas@evident.capital>
2023-03-11 23:17:25 +0530

    Smart Contract Structure complete.
    Adding Functions

---
+++ transactions/run/common_txn.py
@@ -0,0 +1,60 @@
+from algosdk import encoding, atomic_transaction_composer, transaction
+from connection import algo_conn, connect_indexer
+from application import Optimum
+from beaker import client
+
+# Connect to Algod-Client in Testnet Network
+algod_client = algo_conn("testnet")
+indexer_client = connect_indexer("testnet")
+# Create a Dummy Signer to fetch the transaction object
+ACCOUNT_SIGNER = atomic_transaction_composer.AccountTransactionSigner("a" * 32)
+
+
+# Optimum App optin by the wallet
+def app_opt_in(sender_wallet, app_id):
+
+    # Create  an app client for our app
+    app_client = client.ApplicationClient(
+        client=algod_client, app=Optimum(), app_id=app_id, signer=ACCOUNT_SIGNER
+    )
+
+    atc = atomic_transaction_composer.AtomicTransactionComposer()
+    app_client.add_method_call(
+        atc=atc,
+        method=Optimum.opt_in,
+        sender=sender_wallet
+    )
+
+    # extract the transaction from the ATC
+    txn_details = atc.txn_list[0]
+    result = [{'txn': encoding.msgpack_encode(txn_details.txn)}]
+
+    return result
+
+
+# Optimum ASA optin by the wallet
+def asset_opt_in(sender_wallet, asset_id):
+
+    # transaction params
+    params_optin = algod_client.suggested_params()
+
+    # Optin to OPT ASA by fee account
+    atc = atomic_transaction_composer.AtomicTransactionComposer()
+    atc.add_transaction(
+        atomic_transaction_composer.TransactionWithSigner(
+            txn=transaction.AssetTransferTxn(
+                sender_wallet,
+                params_optin,
+                sender_wallet,
+                0,
+                asset_id
+            ),
+            signer=ACCOUNT_SIGNER
+        )
+    )
+
+    # extract the transaction from the ATC
+    txn_details = atc.txn_list[0]
+    result = [{'txn': encoding.msgpack_encode(txn_details.txn)}]
+
+    return result
---
+++ transactions/run/deposit.py
@@ -0,0 +1,66 @@
+from algosdk import encoding, atomic_transaction_composer, transaction, logic
+from connection import algo_conn, connect_indexer
+from application import Optimum
+from beaker import client
+
+# Connect to Algod-Client in Testnet Network
+algod_client = algo_conn("testnet")
+indexer_client = connect_indexer("testnet")
+# Create a Dummy Signer to fetch the transaction object
+ACCOUNT_SIGNER = atomic_transaction_composer.AccountTransactionSigner("a" * 32)
+
+
+def deposit(sender_wallet, app_id, asset_id, algo_amt):
+
+    # Create  an app client for our app
+    app_client = client.ApplicationClient(
+        client=algod_client, app=Optimum(), app_id=app_id, signer=ACCOUNT_SIGNER
+    )
+
+    # deposit ALGO to the contract, receive OPT
+    atc = atomic_transaction_composer.AtomicTransactionComposer()
+
+    # Transaction 1: Pay algos to smart contract
+    params_txn1 = algod_client.suggested_params()
+    app_client.add_transaction(
+        atc=atc,
+        txn=transaction.PaymentTxn(
+            sender=sender_wallet,
+            sp=params_txn1,
+            receiver=logic.get_application_address(app_id),
+            amt=algo_amt
+        )
+    )
+
+    # Transaction 2: Receive OPT, call the Optimum Application
+    params_txn2 = algod_client.suggested_params()
+    params_txn2.fee = 2000
+    app_client.add_method_call(
+        atc=atc,
+        method=Optimum.exchange,
+        sender=sender_wallet,
+        suggested_params=params_txn2,
+        foreign_assets=[asset_id]
+    )
+
+    # fetch the transaction objects
+    txn_1 = atc.txn_list[0]
+    txn_2 = atc.txn_list[1]
+
+    # Group Transactions
+    print("Grouping transactions...")
+    group_id = transaction.calculate_group_id([txn_1, txn_2])
+    print("...computed groupId: ", group_id)
+    txn_1.group = group_id
+    txn_2.group = group_id
+
+    txn_grp = [{'txn': encoding.msgpack_encode(txn_1)},
+               {'txn': encoding.msgpack_encode(txn_2)}]
+
+    return txn_grp
+
+
+# Find and fund custodial wallets with 10000 ALGO increments.
+# Returns if enough wallets are not available.
+# NOTE: deposit amount is in microAlgos
+def fund_custodian_wallets(sender_wallet, asset_id, amt):
